6bd23dac54c08b062521417d8b5de33f714525dc
ReiTulius <heytuliusmusic@gmail.com>
2026-05-26 01:28:09 -0300

    app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.title("💿 Automatizador de Acervo - Tulio Para Udesc FM")
 st.markdown("Insira a lista de músicas baixadas para cadastrar diretamente no Google Sheets de forma 100% gratuita.")
 
 # 🔗 COLOQUE A SUA URL DO APPS SCRIPT AQUI
-URL_ENVIO_GOOGLE = "https://script.google.com/macros/s/AKfycbyVcmF-PVV5sr8eSLUhrisC0WKoQBi0pwT1jQQUJdUnJM-BWEFduPEbOUtAYFe7DtDt/exec"
+URL_ENVIO_GOOGLE = "https://script.google.com/macros/s/AKfycbxZUCCr2jutkgQDQl7_8uNOKCPiMx1EHA9fNKdtt7m4uQqfn2kiq8hu1alXCqItK293/exec"
 
 def processar_linha_musica(linha_bruta):
     linha_bruta = \
